116bb6dde1f214abaa278e8c983871487f7dfdea
Guruansh Kohli <Guruansh.Kohli@gmail.com>
2025-08-01 11:17:18 -0400

    added card animation and disabled buttons properly

--- Monty_2/Monty_2/ContentView.swift
+++ Monty_2/Monty_2/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var monty: Monty
+    @State private var areCardsDealt = false
 
     var body: some View {
         ZStack{
@@ -23,8 +24,8 @@ struct ContentView: View {
                     .opacity(monty.gameOver ? 1 : 0)
                     .animation(.easeIn, value: monty.gameOver)
                 
-                // 3 cards face down in an Hstack
-                HStack(spacing: 20) {
+                // 3 cards animated in a zstack
+                ZStack {
                     ForEach(0..<3, id: \.self) { i in
                         let isFaceUp = monty.cardStates[i] == .faceUp
                         let faceName = i == monty.aceIndex ? "card_ace" : "card_blank"
@@ -33,6 +34,10 @@ struct ContentView: View {
                             contentImageName: isFaceUp ? faceName : "card_ace"
                         )
                         .frame(width: 100, height: 150)
+                        .offset(x: areCardsDealt ? (-120 + CGFloat(i) * 120) : 0)
+                        // animate whenever areCardsDealt toggles
+                        .animation(.easeInOut(duration: 0.5), value: areCardsDealt)
+                        .allowsHitTesting(areCardsDealt)
                         .onTapGesture {
                             monty.onCardTap(at: i)
                         }
@@ -42,7 +47,7 @@ struct ContentView: View {
                 // Creating Control Strip
                 HStack(spacing: 30) {
                     
-                    // Player & House scores
+                    // Player and House scores
                     VStack(alignment: .leading, spacing: 4) {
                         Text("Player: \(monty.playerPoints)")
                             .font(.headline)
@@ -55,6 +60,9 @@ struct ContentView: View {
                     // Reset button
                     Button("Reset") {
                         monty.resetRound()
+                        withAnimation(.easeInOut(duration: 0.5)){
+                            areCardsDealt = false
+                        }
                     }
                     .font(.system(size: 20, weight: .semibold))
                     .disabled(!monty.gameOver)
@@ -62,9 +70,13 @@ struct ContentView: View {
                     // New Game button
                     Button("New Game") {
                         monty.newGame()
+                        withAnimation(.easeInOut(duration: 0.5)){
+                            areCardsDealt = true
+                        }
                     }
                     .font(.system(size: 20, weight: .semibold))
-                    .disabled(monty.gameOver)
+                    // disable button on gameover and when new game starts 
+                    .disabled(monty.gameOver || areCardsDealt)
 
                 }
                 .padding(.vertical, 8)
